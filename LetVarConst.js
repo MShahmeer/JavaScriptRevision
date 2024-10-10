7a2b91d34ff218715e41c16033785f9b3f1eca59
shahmeerhisdu <muhammadshahmeerhisdu@gmail.com>
2024-10-10 16:05:58 +0500

    comment added

---
+++ LetVarConst.js
@@ -0,0 +1,12 @@
+const accountId = 135
+let accountEmail = "muhammadshahmeeronline@gmail.com"
+var accountPassword = "1233366"
+accountCity = "Mianwali"
+
+// accountId = 2 //This will give error
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+// In early days in the javascript there was no concept of the scope, and the variables declared with the var keyword
+// don't have the scope, suppose I have declared var check = 0 in the function and I access and modify that outside that
+// function I can do that so because of this reason now the var is not recommended to use.
