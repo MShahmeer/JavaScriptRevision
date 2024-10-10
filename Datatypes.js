f6f14f5b44154d250dc72ed8504958bf2c47b0c2
shahmeerhisdu <muhammadshahmeerhisdu@gmail.com>
2024-10-10 19:00:47 +0500

    datatypes in js

---
+++ Datatypes.js
@@ -0,0 +1,12 @@
+//Modern JS is different from the old JS, there were no modules, classes, arrow functions etc 
+//To write the code in the new version we use "Use Strict" in our code file but now its also not used
+
+// alert(3+3) this will not run in the node js because we can only run this in browser
+
+//you can get the JS official documentation of ECMA from here https://tc39.es/ecma262/ or MDN
+
+// same as datatypes in programming languages
+
+// null is the standalone value in JS and datatype of null is object, while undefined is datatype
+
+// Symbol is the datatype as well
